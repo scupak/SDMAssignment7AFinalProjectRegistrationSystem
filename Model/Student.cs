fe0bfc29d753a620aaad06c1b75d4997a8478288
kasp608b <54952896+kasp608b@users.noreply.github.com>
2020-10-22 11:14:05 +0200

    Added student properties.

--- Model/Student.cs
+++ Model/Student.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace Core.Services
 {
   public class Student
     {
+        public  int StudentId{ get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string PostalDistrict { get; set; }
+        public string Email { get; set; }
     }
 }
