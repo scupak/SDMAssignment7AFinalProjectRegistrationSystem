5e0eb1f6863789df33c51be55c219d8adaab3ed9
scupak <anto1095@easv365.dk>
2020-10-22 11:41:54 +0200

    Update UnitTest1.cs

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using Core.Interfaces;
+using Core.Services;
+using Moq;
 using Xunit;
 
 namespace XUnitTestProject
@@ -6,6 +10,20 @@ namespace XUnitTestProject
     //Random change. 
     public class UnitTest1
     {
+        // Fake store for repository
+        private List<Student> dataStore;
+        private Mock<IStudentRepository> repoMock;
+
+        public UnitTest1()
+        {
+            dataStore = new List<Student>();
+
+            repoMock = new Mock<IStudentRepository>();
+            repoMock.SetupAllProperties();
+
+            repoMock.Setup(x => x.GetAll()).Returns(() => dataStore.ToArray());
+        }
+
         [Fact]
         public void Test1()
         {
