3636773805b1be1101a13686856b99337c0bd857
scupak <anto1095@easv365.dk>
2020-10-29 09:17:23 +0100

    Create ITeamRepository.cs

---
+++ Core.Interfaces/DomainService/ITeamRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces.DomainService
+{
+   public interface ITeamRepository
+    {
+
+         void Add(Team t);
+         void Update(Team t); 
+         void Remove(Team t);
+         IEnumerable<Team> GetAll();
+         Team GetById(int id); 
+    }
+}
