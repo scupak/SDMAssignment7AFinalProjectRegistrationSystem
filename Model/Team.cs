3a1035bcbb94a18d79724d89a33690f2528d89b4
scupak <anto1095@easv365.dk>
2020-10-29 10:15:43 +0100

    Update Team.cs

--- Model/Team.cs
+++ Model/Team.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Core.Services;
 
 namespace Model
 {
     class Team
     {
+        public int TeamId { get; set;}
+
+        public List<Student>  ListOfStudents { get; set; }
+
+
     }
 }
