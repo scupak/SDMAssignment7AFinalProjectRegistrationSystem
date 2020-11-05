46157788a466cc97dab65624438033fe03dcab41
scupak <anto1095@easv365.dk>
2020-11-05 10:58:40 +0100

    Update IStudentRepository.cs

--- Core.Interfaces/DomainService/IStudentRepository.cs
+++ Core.Interfaces/DomainService/IStudentRepository.cs
@@ -7,7 +7,7 @@ namespace Core.Interfaces
 {
     public interface IStudentRepository
     {
-        //lol
+        //lollol
         IEnumerable<Student> GetAll();
         Student GetById(int id);
         void Add(Student s);
